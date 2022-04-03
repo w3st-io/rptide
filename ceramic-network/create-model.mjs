82b10098366883203bdca26bb976f649bee706dc
aleem ahmed <w3st.io2021@gmail.com>
2022-04-03 15:07:02 -0400

    Improved

---
+++ ceramic-network/create-model.mjs
@@ -0,0 +1,71 @@
+import { writeFile } from 'node:fs/promises'
+import { CeramicClient } from '@ceramicnetwork/http-client'
+import { ModelManager } from '@glazed/devtools'
+import { DID } from 'dids'
+import { Ed25519Provider } from 'key-did-provider-ed25519'
+import { getResolver } from 'key-did-resolver'
+import { fromString } from 'uint8arrays'
+
+
+// The key must be provided as an environment variable
+const key = fromString(process.env.DID_KEY, 'base16')
+
+
+// Create and authenticate the DID
+const did = new DID({
+  provider: new Ed25519Provider(key),
+  resolver: getResolver(),
+})
+await did.authenticate()
+
+
+// [CERAMIC] //
+const ceramic = new CeramicClient('http://localhost:7007')
+
+
+// [CERAMIC-DID] //
+ceramic.did = did
+
+
+// [MODEL-MANAGER] //
+const manager = new ModelManager(ceramic)
+
+
+// [MODEL-MANAGER][CREATE-SCHEMA] //
+const noteSchemaID = await manager.createSchema(
+	'SimpleNote',
+	{
+		$schema: 'http://json-schema.org/draft-07/schema#',
+		title: 'SimpleNote',
+		type: 'object',
+		properties: {
+			text: {
+				type: 'string',
+				title: 'text',
+				maxLength: 4000,
+			},
+		},
+	}
+)
+
+
+// Create the definition using the created schema ID
+
+const whatIsThis = await manager.createDefinition(
+	'myNote',
+	{
+		name: 'My note',
+		description: 'A simple text note',
+		schema: manager.getSchemaURL(noteSchemaID),
+	}
+)
+console.log('whatIsThis', whatIsThis)
+  
+
+// Create a tile using the created schema ID
+const whatIsThis2 = await manager.createTile(
+	'exampleNote',
+	{ text: 'A simple note' },
+	{ schema: manager.getSchemaURL(noteSchemaID) },
+)
+console.log('whatIsThis2', whatIsThis2)
