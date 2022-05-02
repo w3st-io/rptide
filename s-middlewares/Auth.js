2ec7e310268806b760d426f7561f11e7ccd5bb51
aleem-ahmed <w3st.io2021@gmail.com>
2022-05-02 15:34:26 -0400

    Improved

--- s-middlewares/Auth.js
+++ s-middlewares/Auth.js
@@ -5,7 +5,6 @@ const validator = require('validator')
 
 // [REQUIRE] Personal //
 const config = require('../s-config')
-const BanCollection = require('../s-collections/BanCollection')
 const UserCollection = require('../s-collections/UserCollection')
 const h_apiSubscription = require('../s-route-handler/apiSubscription')
 
@@ -15,109 +14,8 @@ const secretKey = config.app.secretKey
 
 
 class Auth {
-	/******************* [ADMIN] *******************/
-	// [ADMIN-TOKEN] //
-	static adminToken() {
-		return (req, res, next) => {
-			// [INIT] //
-			const token = req.headers.admin_authorization
-
-			// If a token exists =>  Validate JWT //
-			if (token) {
-				// [SLICE] "Bearer " //
-				const tokenBody = token.slice(7)
-
-				if (validator.isJWT(tokenBody)) {
-					// [VERIFY] tokenBody //
-					jwt.verify(tokenBody, secretKey, async (err, decoded) => {
-						if (decoded) {
-							// [INIT] Put decoded in req //
-							req.admin_decoded = decoded
-
-							// Check if the role is admin
-							if (decoded.role == 'admin') { next() }
-							else {
-								res.send({
-									executed: true,
-									status: false,
-									location: '/s-middlewares/Auth',
-									message: 'Access Denied: Invalid Token',
-									auth: false,
-								})
-							}
-						}
-						else {
-							res.send({
-								executed: true,
-								status: false,
-								location: '/s-middlewares/Auth',
-								message: `Access Denied: JWT Error --> ${err}`,
-								auth: false,
-							})
-						}
-					})
-				}
-				else {
-					res.send({
-						executed: true,
-						status: false,
-						location: '/s-middlewares/Auth',
-						message: 'Access denied: Not valid JWT',
-						auth: false,
-					})
-				}
-			}
-			else {
-				res.send({
-					executed: true,
-					status: false,
-					location: '/s-middlewares/Auth',
-					message: 'Access denied: No token passed',
-					auth: false,
-				})
-			}
-		}
-	}
-
-
-	// [USER-TOKEN] NOT required //
-	static adminTokenNotRequired() {
-		return async (req, res, next) => {
-			if (req.headers.admin_authorization) {
-				// [SLICE] "Bearer " //
-				const tokenBody = req.headers.admin_authorization.slice(7)
-
-				if (tokenBody !== 'undefined') {
-					// Validate JWT //
-					try {
-						const decoded = await jwt.verify(tokenBody, secretKey)
-
-						// Check if the role is admin
-						if (decoded.role == 'admin') { req.admin_decoded = decoded }
-						else {
-							res.send({
-								executed: true,
-								status: false,
-								location: '/s-middlewares/Auth',
-								message: 'Access Denied: Invalid Token',
-								auth: false,
-							})
-						}
-					}
-					catch (err) { console.log('JWT Verify:', err) }
-					
-				}
-			}
-			else { console.log('no req.headers.user_authorization') }
-			
-			// Since token is not required move on anyways
-			next()
-		}
-	}
-
-
 	/******************* [USER] *******************/
-	// [USER-TOKEN] //
+	// [Standard] //
 	static userToken() {
 		return async (req, res, next) => {
 			// If a token exists => Validate JWT //
@@ -142,11 +40,6 @@ class Auth {
 								)
 
 								if (verified.status) {
-									// Check Ban //
-									const ban = await BanCollection.c_existance(
-										decoded.user_id
-									)
-
 									// Check apiSubscription status //
 									await h_apiSubscription.cycleCheckApiSubscription({
 										user_id: decoded.user_id
@@ -200,7 +93,7 @@ class Auth {
 	}
 
 
-	// [USER-TOKEN] NOT required //
+	// [IF-LOGGED] NOT required //
 	static userTokenNotRequired() {
 		return async (req, res, next) => {
 			// [INIT] //
@@ -236,7 +129,7 @@ class Auth {
 	}
 
 
-	// [USER-TOKEN] Verification NOT required //
+	// [LIMITED] Verification NOT required //
 	static userTokenByPassVerification() {
 		return (req, res, next) => {
 			// [INIT] //
@@ -298,7 +191,7 @@ class Auth {
 	}
 
 
-	// [USER-TOKEN][API-PRIVATE-KEY] //
+	// [API-PRIVATE-KEY] //
 	static userTokenOrAPIPrivateKey() {
 		return async (req, res, next) => {
 			// If a token exists => Validate JWT //
@@ -323,11 +216,6 @@ class Auth {
 								)
 
 								if (verified.status) {
-									// Check Ban //
-									const ban = await BanCollection.c_existance(
-										decoded.user_id
-									)
-									
 									next()
 								}
 								else { res.send(verified) }
