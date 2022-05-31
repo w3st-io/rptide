f575ecb5c37eefd886b6dad914a455af9f674291
aleem-ahmed <w3st.io2021@gmail.com>
2022-05-31 11:27:34 -0400

    Simplify Axios Services

--- client/src/services/PageService.js
+++ client/src/services/PageService.js
@@ -2,25 +2,18 @@
 import axios from 'axios'
 
 
-// [AUTH-TOKEN-SETUP] //
-const authAxios = async () => {
-	return axios.create({
-		baseURL: '/pages',
-		headers: {
-			user_authorization: `Bearer ${localStorage.usertoken}`,
-		}
-	})
-}
+// [AUTH-AXIOS] //
+const authAxios = axios.create({
+	baseURL: '/pages',
+	headers: {
+		user_authorization: `Bearer ${localStorage.usertoken}`,
+	}
+})
 
 export default {
-	authAxios,
-
-
 	// [HOME] //
 	s_home: async function () {
 		try {
-			const authAxios = await this.authAxios()
-	
 			const res = await authAxios.get('/')
 
 			return res.data
@@ -38,8 +31,6 @@ export default {
 	// [ACTIVITY] //
 	s_activity: async function (sort = 0, limit, page) {
 		try {
-			const authAxios = await this.authAxios()
-
 			const res = await authAxios.get(`/activity/index/${sort}/${limit}/${page}`)
 
 			return res.data
@@ -57,8 +48,6 @@ export default {
 	// [ADMIN] //
 	s_admin: async function () {
 		try {
-			const authAxios = await this.authAxios()
-	
 			const res = await authAxios.get('/admin/index')
 
 			return res.data
@@ -75,8 +64,6 @@ export default {
 
 	s_admin_function_commentReports: async function (sort, limit, page) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (
 				await authAxios.get(
 					`/admin/function/commentReports/${sort}/${limit}/${page}`
@@ -95,8 +82,6 @@ export default {
 
 	s_admin_function_comments: async function (sort, limit, page) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (
 				await authAxios.get(`/admin/function/comments/${sort}/${limit}/${page}`)
 			).data
@@ -113,8 +98,6 @@ export default {
 
 	s_admin_function_posts: async function (sort, limit, page) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (
 				await authAxios.get(`/admin/function/posts/${sort}/${limit}/${page}`)
 			).data
@@ -131,8 +114,6 @@ export default {
 
 	s_admin_function_users: async function (sort, limit, page) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (
 				await authAxios.get(`/admin/function/users/${sort}/${limit}/${page}`)
 			).data
@@ -149,8 +130,6 @@ export default {
 
 	s_admin_function_users_record: async function (user_id) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (
 				await authAxios.get(`/admin/function/users/record/${user_id}`)
 			).data
@@ -168,8 +147,6 @@ export default {
 	s_blogPost_read: async function ({ blogPost_id }) {
 		console.log(blogPost_id);
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (await authAxios.get(`/blog-post/read/${blogPost_id}`)).data
 		}
 		catch (err) {
@@ -185,8 +162,6 @@ export default {
 	// [CAT] //
 	s_cat: async function (cat_id, sort = 0, limit, page) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			const res = await authAxios.get(`/cat/index/${cat_id}/${sort}/${limit}/${page}`)
 			
 			return res.data
@@ -204,8 +179,6 @@ export default {
 	// [PRODUCT] //
 	s_product_read: async function ({ product_id }) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			const { data } = await authAxios.get(`/product/read/${product_id}`)
 	
 			return data
@@ -226,8 +199,6 @@ export default {
 	// [POST] //
 	s_post: async function (post_id, limit, page) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			const { data } = await authAxios.get(`/post/index/${post_id}/${limit}/${page}`)
 	
 			return data
@@ -244,8 +215,6 @@ export default {
 
 	s_post_create: async function () {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (await authAxios.get(`/post/create`)).data
 		}
 		catch (err) {
@@ -261,8 +230,6 @@ export default {
 	// [COMMENT] //
 	s_comment_create: async function (post_id) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (await authAxios.get(`/comment/create/${post_id}`)).data
 		}
 		catch (err) {
@@ -277,8 +244,6 @@ export default {
 	
 	s_comment_edit: async function (comment_id) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			const reqData = (await authAxios.get(`/comment/edit/${comment_id}`))
 	
 			return reqData.data
@@ -295,8 +260,6 @@ export default {
 	
 	s_comment_reply: async function (comment_id) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (await authAxios.get(`/comment/reply/${comment_id}`)).data
 		}
 		catch (err) {
@@ -312,8 +275,6 @@ export default {
 	// [FOLLOWED] //
 	s_followed: async function (sort, limit, page) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (
 				await authAxios.get(`/followed/${sort}/${limit}/${page}`)
 			).data
@@ -331,8 +292,6 @@ export default {
 	// [USER] //
 	s_user: async function () {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (
 				await authAxios.get(`/user/index`)
 			).data
@@ -350,8 +309,6 @@ export default {
 	// [DASHBOARD] //
 	s_user_dashboard: async function ({ tab, sort, limit, page }) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			const res = await authAxios.get(
 				`/user/dashboard/index/${tab}/${sort}/${limit}/${page}`
 			)
@@ -370,8 +327,6 @@ export default {
 	
 	s_user_profile: async function () {
 		try {
-			const authAxios = await this.authAxios()
-		
 			return (await authAxios.get('/user/profile/index')).data
 		}
 		catch (err) {
@@ -386,8 +341,6 @@ export default {
 	
 	s_user_profile_edit: async function () {
 		try {
-			const authAxios = await this.authAxios()
-		
 			return (await authAxios.get(`/user/profile/edit`)).data
 		}
 		catch (err) {
@@ -402,8 +355,6 @@ export default {
 	
 	s_user_profile_lookup: async function (user_id) {
 		try {
-			const authAxios = await this.authAxios()
-		
 			return (await authAxios.get(`/user/profile/lookup/${user_id}`)).data
 		}
 		catch (err) {
@@ -419,8 +370,6 @@ export default {
 	// [QUOTE] //
 	s_quote: async function (query) {
 		try {
-			const authAxios = await this.authAxios()
-			
 			return (
 				await authAxios.get(`/quote/${query}`)
 			).data
@@ -438,8 +387,6 @@ export default {
 	// [SEARCH] //
 	s_search: async function (query, type, limit, page) {
 		try {
-			const authAxios = await this.authAxios()
-		
 			return (
 				await authAxios.get(`/search/${query}/${type}/${limit}/${page}`)
 			).data
@@ -455,8 +402,6 @@ export default {
 
 	s_sectionText_read: async function ({ sectionText_id }) {
 		try {
-			const authAxios = await this.authAxios()
-	
 			return (await authAxios.get(`/section-text/read/${sectionText_id}`)).data
 		}
 		catch (err) {
