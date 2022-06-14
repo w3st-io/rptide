b97e2b304fde4febb23ebd75b23eec199f156b95
aleem ahmed <w3st.io2021@gmail.com>
2022-06-14 12:50:27 -0400

    s

--- client/tests/unit/example.spec.js
+++ client/tests/unit/example.spec.js
@@ -1,12 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import WebContent from '../../src/components/dashboard/WebContent.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+describe('WebContent.vue', () => {
+  it(
+    'renders props.webApp when passed',
+    () => {
+      const wrapper = shallowMount(
+        WebContent,
+        {
+          propsData: {
+            webApp
+          }
+        }
+      )
+      expect(wrapper.text()).toMatch('Matches')
+    }
+  )
 })
