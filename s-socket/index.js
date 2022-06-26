98464b250bcf9c25ae4ad115d7276b7c88dd6767
aleem-ahmed <w3st.io2021@gmail.com>
2022-06-25 21:30:48 -0400

    Improved

--- s-socket/index.js
+++ s-socket/index.js
@@ -7,40 +7,12 @@ module.exports = {
 		io.on('connection', (socket) => {
 			// [JOIN] sockets //
 			socketService.joinSockets(socket.id)
-
-
-			// [EMIT] User //
-			//socket.emit('user', socketService.getUserSocket(socket.id))
 		
 
 			// [ON] User Join //
 			socket.on('user-login', (user_id) => {
 				// [EXISTANCE] user_id not in room // 
 				if (user_id) { socketService.joinSocketUsers(socket.id, user_id) }
-			})
-			
-
-			// [ON] asset subscribe //
-			socket.on('asset-subscribe', (exchange, product_id, timeFrame) => {
-				socket.join(`${exchange}-${product_id}`)
-				socket.join(`${exchange}-${timeFrame}`)
-
-				io.to(`${exchange}-${product_id}`).emit(
-					'asset-data',
-					'connection establised'
-				)
-
-				socketService.joinSocketsCoinbase({
-					socket_id: socket.id,
-					exchange: exchange,
-					product_id: product_id
-				})
-			})
-
-
-			// [ON] asset unsubscribe //
-			socket.on('asset-unsubscribe', () => {
-				socketService.leaveSocketsCoinbase({ socket_id: socket.id, })
 			})
 
 		
