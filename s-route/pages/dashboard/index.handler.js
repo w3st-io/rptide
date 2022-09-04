d6045087510bac9bfe5d6ba6dd2e5dfceed1ba35
aleem ahmed <w3st.io2021@gmail.com>
2022-09-04 15:53:29 -0400

    s

---
+++ s-route/pages/dashboard/index.handler.js
@@ -0,0 +1,111 @@
+// [REQUIRE] //
+const validator = require('validator')
+
+
+// [REQUIRE] Personal //
+const ProductOptionCollection = require('../../../s-collections/ProductOptionCollection')
+const ProductCollection = require('../../../s-collections/ProductCollection')
+const config_const = require('../../../s-config/const')
+const h_apiSubscription = require('../../api/user/api-subscription.handler')
+
+
+// [CONST] //
+const location = '/pages/dashboard/index';
+
+
+module.exports = {
+	index: async ({ req }) => {
+		try {
+			if (
+				validator.isAscii(req.params.tab) ||
+				validator.isAscii(req.params.sort) ||
+				validator.isAscii(req.params.page) ||
+				validator.isAscii(req.params.limit)
+			) {
+				return {
+					executed: true,
+					status: false,
+					location: location,
+					message: 'Invalid Params'
+				};
+			}
+
+			// [INIT] //
+			const sort = parseInt(req.params.sort);
+			const limit = parseInt(req.params.limit);
+			const pageIndex = parseInt(req.params.page) - 1;
+			const skip = pageIndex * limit;
+
+			const apiSubscriptionTier = await h_apiSubscription.getSubscriptionTier({
+				user_id: req.user_decoded._id,
+			});
+
+			switch (req.params.tab) {
+				case 'web-app':
+					return {
+						executed: true,
+						status: true,
+						location: location,
+					};
+				break
+
+				case 'api':
+					return {
+						executed: true,
+						status: true,
+						location: location,
+					};
+				break;
+
+				case 'web-content':
+					return {
+						executed: true,
+						status: true,
+						location: location,
+					};
+				break;
+
+				case 'product':
+					const productsObj = await ProductCollection.c_readAll_sorted_byUser({
+						user_id: req.user_decoded._id,
+					})
+
+					productsObj.limit = config_const.limit
+					productsObj.apiSubscriptionTier = apiSubscriptionTier
+
+					return productsObj;
+				break;
+
+				case 'product-options':
+					const productOptionssObj = await ProductOptionCollection.c_readAll_sorted_byUser({
+						user_id: req.user_decoded._id,
+					});
+
+					productOptionssObj.limit = config_const.limit;
+					productOptionssObj.apiSubscriptionTier = apiSubscriptionTier;
+
+					return productOptionssObj;
+				break;
+
+				
+				default:
+					return {
+						executed: true,
+						status: false,
+						location: location,
+						message: 'Invalid tab'
+					};
+				break;
+			}
+
+		}
+		catch (err) {
+			return {
+				executed: false,
+				status: false,
+				location: location,
+				message: `Error --> ${err}`
+			};
+		}
+	}
+}
--- s-route/pages/dashboard/index.js
+++ s-route/pages/dashboard/index.js
@@ -1,127 +1,23 @@
 // [REQUIRE] //
-const cors = require('cors')
-const express = require('express')
-const validator = require('validator')
+const cors = require('cors');
+const express = require('express');
 
 
 // [REQUIRE] Personal //
-const ProductOptionCollection = require('../../../s-collections/ProductOptionCollection')
-const ProductCollection = require('../../../s-collections/ProductCollection')
-const UserCollection = require('../../../s-collections/UserCollection')
-const config_const = require('../../../s-config/const')
-const h_apiSubscription = require('../../api/user/api-subscription.handler')
-const Auth = require('../../../s-middlewares/Auth')
+const rh = require('./index.handler');
+const Auth = require('../../../s-middlewares/Auth');
 
 
 // [EXPRESS + USE] //
-const router = express.Router().use(cors())
-
-
-// [CONST] //
-const location = '/pages/dashboard/index'
+const router = express.Router().use(cors());
 
 
 // [SEARCH] //
 router.get(
 	'/index/:webapp/:tab/:sort/:limit/:page',
 	Auth.userToken(),
-	async (req, res) => {
-		try {
-			console.log(req.params);
-			if (
-				validator.isAscii(req.params.tab) &&
-				validator.isAscii(req.params.sort) &&
-				validator.isAscii(req.params.page) &&
-				validator.isAscii(req.params.limit)
-			) {
-				// [INIT] //
-				const sort = parseInt(req.params.sort)
-				const limit = parseInt(req.params.limit)
-				const pageIndex = parseInt(req.params.page) - 1
-				const skip = pageIndex * limit
-
-				const apiSubscriptionTier = await h_apiSubscription.getSubscriptionTier({
-					user_id: req.user_decoded._id,
-				})
-
-				switch (req.params.tab) {
-					case 'web-app':
-						res.send({
-							executed: true,
-							status: true,
-							location: location,
-						})
-					break
-
-					case 'api':
-						res.send({
-							executed: true,
-							status: true,
-							location: location,
-						})
-					break
-
-					case 'web-content':
-						res.send({
-							executed: true,
-							status: true,
-							location: location,
-						})
-					break
-
-					case 'product':
-						const productsObj = await ProductCollection.c_readAll_sorted_byUser({
-							user_id: req.user_decoded._id,
-						})
-
-						productsObj.limit = config_const.limit
-						productsObj.apiSubscriptionTier = apiSubscriptionTier
-
-						res.send(productsObj)
-					break
-
-					case 'product-options':
-						const productOptionssObj = await ProductOptionCollection.c_readAll_sorted_byUser({
-							user_id: req.user_decoded._id,
-							
-						})
-
-						productOptionssObj.limit = config_const.limit
-						productOptionssObj.apiSubscriptionTier = apiSubscriptionTier
-
-						res.send(productOptionssObj)
-					break
-
-					
-					default:
-						res.send({
-							executed: true,
-							status: false,
-							location: location,
-							message: 'Invalid tab'
-						})
-					break
-				}
-			}
-			else {
-				res.send({
-					executed: true,
-					status: false,
-					location: location,
-					message: 'Invalid Params'
-				})
-			}
-		}
-		catch (err) {
-			res.send({
-				executed: false,
-				status: false,
-				location: location,
-				message: `Error --> ${err}`
-			})
-		}
-	}
-)
+	async (req, res) => { res.send(await rh.index({ res })); }
+);
 
 
-module.exports = router
+module.exports = router;
