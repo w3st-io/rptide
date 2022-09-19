f7ce6c01fa84c7b9110a323f52194f869a5030ad
aleem ahmed <w3st.io2021@gmail.com>
2022-09-19 15:17:18 -0400

    Improved

--- client/src/components/dashboard/WebContent.vue
+++ client/src/components/dashboard/WebContent.vue
@@ -6,17 +6,18 @@
 			</BCol>
 
 			<BCol cols="12" md="3">
-				<router-link to="/web-content/create">
+				<RouterLink to="/web-content/create">
 					<BButton
 						class="w-100 mb-3"
 						variant="success"
 						pill
 					>+ Create</BButton>
-				</router-link> 
+				</RouterLink> 
 			</BCol>
 
 			<BCol
-				v-for="(w,i) in webContents" :key="i"
+				v-for="(w,i) in webContents"
+				:key="i"
 				cols="12" md="6" lg="4"
 				class="d-flex align-items-stretch mb-3"
 			>
@@ -25,14 +26,22 @@
 					class="w-100 text-decoration-none"
 				>
 					<BCard
+						no-body
 						bg-variant="dark"
 						text-variant="light"
 						border-variant="secondary"
 						class="h-100 mb-3 hover-card"
 					>
-						<h5 class="text-primary">{{ w.name }}</h5>
-						<h6 lass="m-0">{{ w.createdTimeStamp }}</h6>
-						<h6 class="m-0 small text-muted">{{ w._id }}</h6>
+						<BCardHeader class="border-secondary">
+							<h5 class="m-0 text-primary">{{ w.name }}</h5>
+						</BCardHeader>
+
+						<BCardBody>
+							<h6 lass="m-0">
+								{{ new Date(w.createdTimeStamp).toLocaleString() }}
+							</h6>
+							<h6 class="m-0 small text-muted">{{ w._id }}</h6>
+						</BCardBody>
 					</BCard>
 				</RouterLink>
 			</BCol>
@@ -41,7 +50,7 @@
 </template>
 
 <script>
-	import axios from 'axios'
+	import axios from 'axios';
 
 	export default {
 		data() {
@@ -56,21 +65,21 @@
 				resData: {},
 				webContents: [],
 				error: '',
-			}
+			};
 		},
 
 		async created() {
 			try {
 				this.resData = await this.authAxios.post('/find', {
 					webApp: this.$store.state.user.workspace.webApp
-				})
+				});
 
 				if (this.resData.status) {
-					this.webContents = this.resData.data.webContents
+					this.webContents = this.resData.data.webContents;
 				}
 			}
 			catch (err) {
-				this.error = err
+				this.error = err;
 			}
 		},
 	}
