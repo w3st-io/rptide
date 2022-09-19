dd9a5329412138a40bbbdce9e2194e95d8646295
aleem ahmed <w3st.io2021@gmail.com>
2022-09-19 14:57:09 -0400

    Make code more java like

--- s-socket/index.js
+++ s-socket/index.js
@@ -1,32 +1,36 @@
 // [REQUIRE] Personal
-const socketService = require('./socketService')
+const socketService = require("./socketService");
 
 
 module.exports = {
 	start: (io) => {
-		io.on('connection', (socket) => {
+		io.on("connection", (socket) => {
 			// [JOIN] sockets
-			socketService.joinSockets(socket.id)
+			socketService.joinSockets(socket.id);
 		
 
 			// [ON] User Join
 			socket.on(
-				'user-login',
+				"user-login",
 				(user_id) => {
 					// [EXISTANCE] user_id not in room // 
 					if (user_id) {
-						socketService.joinSocketUsers(socket.id, user_id)
+						socketService.joinSocketUsers(socket.id, user_id);
 					}
 				}
-			)
+			);
 
 		
 			// [ON] User Leave
-			socket.on('user-logout', () => { socketService.userLogout(socket.id) })
+			socket.on("user-logout", () => {
+				socketService.userLogout(socket.id);
+			});
 		
 		
 			// [ON] Disconnect
-			socket.on('disconnect', () => { socketService.disconnected(socket.id) })
+			socket.on("disconnect", () => {
+				socketService.disconnected(socket.id);
+			});
 		})
 	},
 }
